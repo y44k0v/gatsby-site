45d6b5c9c57a0cdc5d227421ff75424ba01c6c77
y44k0v <ykvgm001@gmail.com>
2024-08-02 02:11:26 -0400

    clean slate

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Adding about page to my gatsby site</p>
+        </>
+    )
+}
+
+export default About
+
+export const Head = () => <title>About</title>
